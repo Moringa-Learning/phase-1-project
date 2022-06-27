f7f7a6bb243d1b09c3ce5264cc781f8a896e9887
layersony <layersony@gmail.com>
2022-06-27 21:06:48 +0300

    initialized and completed Project

---
+++ README.md
@@ -0,0 +1,72 @@
+# Phase 1 Project
+
+
+#### By Samuel Maingi Mutunga
+
+## Description 
+A simple Project that helps developers showcase their skills and also projects done.
+
+---
+
+### Setup
+To access this project on your local files, you can clone it using these steps
+1. Open your terminal
+1. Use this command to clone `$ git clone https://github.com/layersony/phase-1-project.git`
+1. This will clone the repositoty into your local folder
+
+#### Run the Project Locally
+* Follow the Steps below to Access the Project
+
+```bash
+cd phase-1-project
+code phase-1-project
+```
+Make sure you have an extension for Live Server in which you'll use to run the project from
+
+Activate the server
+```bash
+json-server --watch data/db.json 
+```
+
+ __Enjoy :)__
+
+---
+
+## Behaviour Driven Development
+
+1. Project Saved Successfully
+   - INPUT: All the Required Inputs are Entered
+   - OUTPUT: Alert From Javascript with a project saved Successfully
+
+---
+
+## Known Bugs
+
+* None at the Moment
+
+## Technologies Used
+HTML
+
+CSS
+
+Javascript
+
+Bootstrap
+
+Git
+
+[Json-server](https://www.npmjs.com/package/json-server)
+
+Virtual Studio Code
+
+---
+
+## Contact Details
+sammaingi5@gmail.com
+
+Can View My Portfolio --> [Click_ME](https://samuelmaingi.herokuapp.com/)
+
+---
+
+### License
+This Project is under the [MIT](LICENCE) license
---
+++ assets/js/script.js
@@ -0,0 +1,79 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const projectUrl = "http://localhost:3000/project"
+
+  // containers
+  let projectContainer = document.getElementById('projectContainer')
+
+  // get projects from server
+  function getProjects() {
+    let reqOptions = {
+      method: 'GET'
+    }
+
+    fetch(projectUrl, reqOptions)
+      .then((response) => response.json())
+      .then(results => {
+        results.forEach(proitem => {
+          displayProject(proitem)
+        })
+      })
+  }
+
+  // display card project
+  function displayProject(itemPro) {
+    const proCard = `
+        <div class="col-4">
+          <div class="proCont">
+            <img src="${itemPro.image}" alt="">
+            <h3>${itemPro.title}</h3>
+            <a href = "${itemPro.livesite}" > Live Site </a>
+            <a href = "${itemPro.gitRepo}" > Github Repo </a>
+            <div class="languages">
+              ${itemPro.language.split(',').map(item => `<span>${item}</span>`).join('')}
+            </div>
+          </div>
+        </div>
+      `
+    projectContainer.innerHTML += proCard
+  }
+
+
+  const addproForm = document.querySelector('#addproject')
+
+  if (addproForm !== null) {
+    addproForm.addEventListener('submit', (e) => {
+      e.preventDefault()
+      console.log(e.target.imageUrl.value)
+
+      var requestOptions = {
+        method: 'POST',
+        body: JSON.stringify({
+          id: 0,
+          image: e.target.imageUrl.value,
+          title: e.target.title.value,
+          livesite: e.target.livelink.value,
+          gitRepo: e.target.gitrepo.value,
+          language: e.target.languanges.value
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      };
+
+
+      fetch(projectUrl, requestOptions)
+        .then(response => response.text())
+        .then(result => {
+          alert('Project Saved Successfully')
+          console.log(result)
+          window.location.reload();
+        })
+        .catch(error => {
+          alert('Saving Unsuccessfully')
+          console.log('error', error)
+        });
+    })
+  }
+
+  getProjects()
+})
